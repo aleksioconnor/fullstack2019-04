b9e6d272df5714d6194e2b9e3c22d7ba125d563b
aleksioconnor <aleksi.oconnor@gmail.com>
2019-05-08 11:49:31 +0300

    4.15

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(cors())
 
 app.use((error, request, response, next) => {
